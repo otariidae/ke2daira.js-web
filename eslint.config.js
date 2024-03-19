797262c14836ca973b58c829ac19ce464bfc6a14
otariidae <otariidae@users.noreply.github.com>
2024-03-19 22:33:02 +0900

    refactor: use flat config for ESLint

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+const eslint = require("@eslint/js")
+const tseslint = require("typescript-eslint")
+const reactRecommended = require('eslint-plugin-react/configs/recommended');
+const eslintConfigPrettier = require("eslint-config-prettier");
+const globals = require("globals")
+
+module.exports = tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.recommended,
+	reactRecommended,
+	{
+		files: ["*.js"],
+		ignores: ["src/**/*"],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			}
+		},
+		rules: {
+			"@typescript-eslint/no-var-requires": "off",
+		}
+		
+	},
+	eslintConfigPrettier,
+)
