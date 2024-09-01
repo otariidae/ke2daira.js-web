9c5526b187d93ca054613cbb23b5e3c10083acfd
otariidae <otariidae@users.noreply.github.com>
2024-09-01 21:51:39 +0900

    refactor: webpack -> rsbuild (#34)
    * refactor: use rsbuild instead of webpack
    
    * deps: drop unused direct dev dependencies

---
+++ rsbuild.config.js
@@ -0,0 +1,18 @@
+import { defineConfig, rspack } from "@rsbuild/core";
+import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
+
+export default defineConfig({
+  plugins: [pluginNodePolyfill()],
+  tools: {
+    rspack: {
+      plugins: [
+        new rspack.CopyRspackPlugin({
+          patterns: [{ from: "node_modules/kuromoji/dict", to: "dict" }],
+        }),
+      ],
+    },
+  },
+  html: {
+    template: "src/index.html",
+  },
+});
