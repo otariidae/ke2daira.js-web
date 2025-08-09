430979f2ddc5332e149270ae6244cae7141b7dc8
otariidae <otariidae@users.noreply.github.com>
2025-08-09 17:34:00 +0900

    refactor: do not use testid (#102)
    * refactor: do not use testid
    
    * test: fix lint errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,34 +1,26 @@
 import React from "react";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { rs, describe, it, expect } from "@rstest/core";
+import { rs, describe, it } from "@rstest/core";
 import App from "./App";
 
 rs.mock("ke2daira", () => import("../__mocks__/ke2daira"));
 
 describe("ke2daira", () => {
-  it("empty", async () => {
-    render(<App />);
-    const $output = screen.getByTestId("ke2daira-output");
-
-    await waitFor(() => expect($output).toHaveTextContent(""));
-  });
   it("松平健", async () => {
     render(<App />);
-    const $input = screen.getByTestId("name-input");
-    const $output = screen.getByTestId("ke2daira-output");
+    const $input = screen.getByPlaceholderText("例：松平 健");
 
     await userEvent.type($input, "松平 健");
 
-    await waitFor(() => expect($output).toHaveTextContent("ケツダイラ マン"));
+    await screen.findByText("ケツダイラ マン");
   });
   it("草刈正雄", async () => {
     render(<App />);
-    const $input = screen.getByTestId("name-input");
-    const $output = screen.getByTestId("ke2daira-output");
+    const $input = screen.getByPlaceholderText("例：松平 健");
 
     await userEvent.type($input, "草刈 正雄");
 
-    await waitFor(() => expect($output).toHaveTextContent("マサカリ クサオ"));
+    await screen.findByText("マサカリ クサオ");
   });
 });
